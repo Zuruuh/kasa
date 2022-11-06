0d0247dc6041f955d8023ef01ef75fbf522da79c
Zuruuh <ziadi.mail.pro@gmail.com>
2022-11-06 22:13:58 +0100

    ✨ Created homepage Hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
 import { useFetch } from '../hooks/useFetch';
 import type { Housing } from '../types/housing/Housing';
+import styles from './Home.module.scss';
+import heroImage from '/assets/hero.jpeg';
 
 const Home: React.FC = () => {
   const housings = useFetch<Housing[]>('/data/housings.json');
 
-  return <div>{housings ? 'Data loaded!' : 'fetching...'}</div>;
+  // return <div>{housings ? 'Data loaded!' : 'fetching...'}</div>;
+  return (
+    <main className="page">
+      <section>
+        <div className={styles.hero}>
+          <img
+            className={styles.heroImage}
+            src={heroImage}
+            alt="Chez vous, partout et ailleurs"
+          />
+          <h2 className={styles.heroHeading}>Chez vous, partout et ailleurs</h2>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export const URI = '/';
