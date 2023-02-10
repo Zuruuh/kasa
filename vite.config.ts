30e4c5d4f6df0a5654985dfe4114a9e021c69398
Zuruuh <ziadi.mail.pro@gmail.com>
2023-02-10 14:51:38 +0100

    ➕ ✅ Add testing libraries vitest, chai, testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
+/// <reference types="vitest" />
+/// <reference types="node" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import path from 'node:path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    globals: false,
+    environment: 'jsdom',
+    silent: false,
+    setupFiles: ['./bootstrap.tsx'],
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
 });
