03d50d0d5ad408a60f3b52348c3bafb90e92279f
Zuruuh <ziadi.mail.pro@gmail.com>
2023-02-10 14:53:50 +0100

    ✨ Use 404 page as a route instead of an error

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
+import { type FC } from 'react';
 import { RouterProvider } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage, { URI as HomeURI } from './pages/home/HomePage';
 import AboutPage, { URI as AboutURI } from './pages/about/AboutPage';
-import Layout from './components/layout/Layout';
+import Layout from './shared/layout/Layout';
 import Error404Page from './pages/errors/404/Error404Page';
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
-    errorElement: <Error404Page />,
     children: [
       {
         path: HomeURI,
         element: <HomePage />,
+        index: true,
       },
       {
         path: AboutURI,
         element: <AboutPage />,
       },
+      {
+        path: '*',
+        element: <Error404Page />,
+      },
     ],
   },
 ]);
 
-const App: React.FC = () => {
+const App: FC = () => {
   return <RouterProvider router={router} />;
 };
 
--- src/pages/errors/404/Error404Page.tsx
+++ src/pages/errors/404/Error404Page.tsx
@@ -1,21 +1,18 @@
 import { Link } from 'react-router-dom';
-import { URI as HomeURI } from '../../home/HomePage';
-import Layout from '../../../components/layout/Layout';
+import { URI as HomeURI } from '~/pages/home/HomePage';
 import styles from './Error404Page.module.scss';
 
 const Error404Page: React.FC = () => {
   return (
-    <Layout>
-      <div className={styles.container}>
-        <span className={styles.title}>404</span>
-        <span className={styles.subtitle}>
-          Oups! La page que vous demandez n'existe pas.
-        </span>
-        <Link to={HomeURI} className={styles.link}>
-          Retourner sur la page d'accueil
-        </Link>
-      </div>
-    </Layout>
+    <div className={styles.container}>
+      <span className={styles.title}>404</span>
+      <span className={styles.subtitle}>
+        Oups! La page que vous demandez n'existe pas.
+      </span>
+      <Link to={HomeURI} className={styles.link}>
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
   );
 };
 
