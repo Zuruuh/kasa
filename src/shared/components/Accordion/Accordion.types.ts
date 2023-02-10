0f81fa465387d38fe077db4276ad67c269868163
Zuruuh <ziadi.mail.pro@gmail.com>
2023-02-10 14:50:22 +0100

    ✨ Accordion component

---
+++ src/shared/components/Accordion/Accordion.types.ts
@@ -0,0 +1,17 @@
+const ACCORDION_STATES = {
+  OPENED: 'opened',
+  CLOSED: 'closed',
+} as const;
+
+Object.freeze(ACCORDION_STATES);
+
+export { ACCORDION_STATES };
+
+export type AccordionState =
+  typeof ACCORDION_STATES[keyof typeof ACCORDION_STATES];
+
+export interface AccordionProps {
+  label: string;
+  content: string;
+  defaultState?: AccordionState;
+}
