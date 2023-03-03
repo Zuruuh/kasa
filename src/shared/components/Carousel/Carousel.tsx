4d6cb886e9045f4e18bb7d61d79cd8bc61af8b49
Zuruuh <ziadi.mail.pro@gmail.com>
2023-03-03 16:10:07 +0100

    ✨ Finish behaviour of Carousel component

--- src/shared/components/Carousel/Carousel.tsx
+++ src/shared/components/Carousel/Carousel.tsx
@@ -1,7 +1,11 @@
-import { useState, type FC } from 'react';
+import { useState, type FC, useMemo, useCallback, useEffect } from 'react';
 import type { AutoScrollableOptions } from '~/shared/types/AutoScrollableOptions';
 import type { URL } from '~/shared/types/URL';
 import styles from './Carousel.module.scss';
+import IconButton from '../IconButton/IconButton';
+import { ReactComponent as LeftArrowIcon } from './assets/left-arrow.svg';
+import { ReactComponent as RightArrowIcon } from './assets/right-arrow.svg';
+import PropTypes, { type Validator } from 'prop-types';
 
 export type CarouselProps = {
   images: URL[];
@@ -11,17 +15,30 @@ export type CarouselProps = {
 
 const Carousel: FC<CarouselProps> = ({
   images,
-  //   showIndex = true,
+  showIndex = true,
   //   scrollOptions = { enabled: true, timeoutInMs: 5000 },
 }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [index, setIndex] = useState<number>(0);
-  const framedImages = [
-    images.at(index - 1),
-    images.at(index),
-    images.at(-(images.length - index - 1)),
-  ];
-  console.log(framedImages);
+  const framedImages = useMemo(
+    () => [
+      images.at(index - 1),
+      images.at(index),
+      images.at(-(images.length - index - 1)),
+    ],
+    [images, index]
+  );
+
+  const next = useCallback(() => {
+    setIndex((i) => (i + 1 > images.length - 1 ? 0 : i + 1));
+  }, [images]);
+
+  const previous = useCallback(() => {
+    setIndex((i) => (i - 1 < 0 ? images.length - 1 : i - 1));
+  }, [images]);
+
+  useEffect(() => {
+    console.log(index);
+  }, [index]);
 
   return (
     <div className={styles.carousel}>
@@ -33,14 +50,34 @@ const Carousel: FC<CarouselProps> = ({
             aria-hidden={index === i}
             src={image}
             style={{
-              '--data-offset': 400 * i,
-              accentColor: '-moz-initial',
+              '--data-offset': `${100 * i}%`,
             }}
-            key={image}
+            key={`${image}-${i}`}
           />
         ))}
+        <div className={styles.controlsContainer}>
+          <div className={`${styles.leftButton} ${styles.controlsButton}`}>
+            <IconButton onClick={previous} icon={<LeftArrowIcon />} />
+          </div>
+          <div className={`${styles.rightButton} ${styles.controlsButton}`}>
+            <IconButton onClick={next} icon={<RightArrowIcon />} />
+          </div>
+          {showIndex ? (
+            <span className={styles.indexContainer}>
+              {index + 1}/{images.length}
+            </span>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </div>
   );
 };
+
+Carousel.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string as Validator<URL>).isRequired,
+  showIndex: PropTypes.bool,
+};
+
 export default Carousel;
