d7e6369a8e9167326c2af52f09f433a8a84f7451
Zuruuh <ziadi.mail.pro@gmail.com>
2023-03-05 14:43:27 +0100

    ✨ New Housing Page

--- src/pages/Housing/HousingPage.tsx
+++ src/pages/Housing/HousingPage.tsx
@@ -1,21 +1,22 @@
 import { type FC } from 'react';
 import { useLoaderData } from 'react-router';
 import StarsRating, { StarsRatingRange } from '~/shared/components/StarsRating';
-import Tag from '~/shared/components/Tag/Tag';
+import Tag from '~/shared/components/Tag';
 import type { Page, HasUriGenerator } from '~/shared/types/Page';
 import { Housing } from '~/shared/types/housing/Housing';
 import Error404Page from '~/pages/Errors/404/Error404Page';
 import Carousel from '~/shared/components/Carousel/Carousel';
+import Accordion, {
+  ACCORDION_SIZES,
+  ACCORDION_STATES,
+} from '~/shared/components/Accordion';
+import styles from './HousingPage.module.scss';
 
 const HousingPage: FC = () => {
   const housing = useLoaderData() as Housing;
 
   return (
     <section>
-      {housing.tags.map((tag) => (
-        <Tag label={tag} key={tag} />
-      ))}
-      <StarsRating count={Number(housing.rating) as StarsRatingRange} />
       <div>
         <Carousel
           images={housing.pictures}
@@ -23,6 +24,51 @@ const HousingPage: FC = () => {
           scrollOptions={{ enabled: true, timeoutInMs: 5000 }}
         />
       </div>
+      <header className={styles.header}>
+        <div className={styles.titleBlock}>
+          <h3 className={styles.title}>{housing.title}</h3>
+          <span className={styles.location}>{housing.location}</span>
+        </div>
+        <div className={styles.host}>
+          <span className={styles.hostName}>{housing.host.name}</span>
+          <img
+            className={styles.hostPicture}
+            src={housing.host.picture}
+            alt={housing.host.name}
+          />
+        </div>
+      </header>
+      <div className={styles.subtitle}>
+        <div className={styles.tags}>
+          {housing.tags.map((tag) => (
+            <Tag label={tag} key={tag} />
+          ))}
+        </div>
+        <div className={styles.rating}>
+          <StarsRating count={Number(housing.rating) as StarsRatingRange} />
+        </div>
+      </div>
+      <section className={styles.informations}>
+        <Accordion
+          label="Description"
+          defaultState={ACCORDION_STATES.OPENED}
+          size={ACCORDION_SIZES.SMALL}
+          content={housing.description}
+        />
+        <Accordion
+          label="Equipements"
+          size={ACCORDION_SIZES.SMALL}
+          defaultState={ACCORDION_STATES.OPENED}
+        >
+          <ul>
+            {housing.equipments.map((equipment) => (
+              <li className={styles.equipment} key={equipment}>
+                {equipment}
+              </li>
+            ))}
+          </ul>
+        </Accordion>
+      </section>
     </section>
   );
 };
