2cf8034a76a2407ba0a168d4ccc9e7b13a5f22ed
Zuruuh <ziadi.mail.pro@gmail.com>
2023-03-06 18:31:52 +0100

    ✨ Make carousel compatible with left/right arrow keys

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,6 +25,7 @@ module.exports = {
     'react/react-in-jsx-scope': ['off'],
     'react/no-unescaped-entities': ['off'],
     '@typescript-eslint/no-non-null-assertion': ['off'],
+    'jsx-a11y/no-noninteractive-tabindex': ['off'],
   },
   settings: {
     react: {
--- src/shared/components/Carousel/Carousel.tsx
+++ src/shared/components/Carousel/Carousel.tsx
@@ -1,10 +1,17 @@
-import { useState, type FC, useMemo } from 'react';
+import {
+  useState,
+  type FC,
+  useMemo,
+  useEffect,
+  useCallback,
+  useRef,
+} from 'react';
 import type { AutoScrollableOptions } from '~/shared/types/AutoScrollableOptions';
 import type { URL } from '~/shared/types/URL';
 import IconButton from '../IconButton/IconButton';
 import { ReactComponent as LeftArrowIcon } from './assets/left-arrow.svg';
 import { ReactComponent as RightArrowIcon } from './assets/right-arrow.svg';
-import PropTypes, { type Validator } from 'prop-types';
+import PropTypes from 'prop-types';
 import { useTimeout } from '~/shared/hooks/useTimeout';
 import styles from './Carousel.module.scss';
 
@@ -19,6 +26,7 @@ const Carousel: FC<CarouselProps> = ({
   showIndex = true,
   scrollOptions = { enabled: true, timeoutInMs: 5000 },
 }) => {
+  const carouselRef = useRef<HTMLDivElement>(null);
   const [index, setIndex] = useState<number>(0);
   const framedImages = useMemo(
     () => [
@@ -29,58 +37,90 @@ const Carousel: FC<CarouselProps> = ({
     [images, index]
   );
 
-  function next() {
-    setIndex((i) => (i + 1 > images.length - 1 ? 0 : i + 1));
-    resetTimeout();
-  }
-
-  function previous(): void {
-    setIndex((i) => (i - 1 < 0 ? images.length - 1 : i - 1));
-    resetTimeout();
-  }
-
   const { reset: resetTimeout } = useTimeout(
-    next,
+    () => next(),
     scrollOptions.enabled ? scrollOptions.timeoutInMs : false
   );
 
+  const next = useCallback(() => {
+    setIndex((i) => (i + 1 > images.length - 1 ? 0 : i + 1));
+    resetTimeout();
+  }, [resetTimeout, images]);
+
+  const previous = useCallback((): void => {
+    setIndex((i) => (i - 1 < 0 ? images.length - 1 : i - 1));
+    resetTimeout();
+  }, [resetTimeout, images]);
+
+  const onKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      e.preventDefault();
+      const callable = {
+        ArrowLeft: previous,
+        ArrowRight: next,
+      }[e.key];
+
+      callable?.();
+    },
+    [next, previous]
+  );
+
+  useEffect(() => {
+    const carousel = carouselRef.current!;
+    carousel.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      carousel.removeEventListener('keydown', onKeyDown);
+    };
+  }, [onKeyDown, carouselRef]);
+
   return (
-    <div className={styles.carousel}>
-      <div className={styles.images}>
-        {framedImages.map((image, i) => (
-          <img
-            className={styles.image}
-            alt={`Illustration de la location (${i + 1}/${images.length})`}
-            aria-hidden={index === i}
-            src={image}
-            style={{
-              '--data-offset': `${100 * i}%`,
-            }}
-            key={`${image}-${i}`}
-          />
-        ))}
-        <div className={styles.controlsContainer}>
-          <div className={`${styles.leftButton} ${styles.controlsButton}`}>
-            <IconButton onClick={previous} icon={<LeftArrowIcon />} />
+    <>
+      <div className={styles.carousel} ref={carouselRef} tabIndex={0}>
+        <div className={styles.images}>
+          {framedImages.map((image, i) => (
+            <img
+              className={styles.image}
+              alt={`Illustration de la location (${i + 1}/${images.length})`}
+              aria-hidden={index !== i}
+              src={image}
+              style={{
+                '--data-offset': `${100 * i}%`,
+              }}
+              key={`${image}-${i}`}
+            />
+          ))}
+          <div className={styles.controlsContainer}>
+            <div className={`${styles.leftButton} ${styles.controlsButton}`}>
+              <IconButton
+                onClick={previous}
+                icon={<LeftArrowIcon />}
+                alt="Swipe to previous image"
+              />
+            </div>
+            <div className={`${styles.rightButton} ${styles.controlsButton}`}>
+              <IconButton
+                onClick={next}
+                alt="Swipe to next image"
+                icon={<RightArrowIcon />}
+              />
+            </div>
+            {showIndex ? (
+              <span className={styles.indexContainer}>
+                {index + 1}/{images.length}
+              </span>
+            ) : (
+              <></>
+            )}
           </div>
-          <div className={`${styles.rightButton} ${styles.controlsButton}`}>
-            <IconButton onClick={next} icon={<RightArrowIcon />} />
-          </div>
-          {showIndex ? (
-            <span className={styles.indexContainer}>
-              {index + 1}/{images.length}
-            </span>
-          ) : (
-            <></>
-          )}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
 Carousel.propTypes = {
-  images: PropTypes.arrayOf(PropTypes.string as Validator<URL>).isRequired,
+  images: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
   showIndex: PropTypes.bool,
 };
 
